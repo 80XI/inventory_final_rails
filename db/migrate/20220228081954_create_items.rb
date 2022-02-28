df9e0eb7d641894126739af9e11863b6d47aa98f
casper <salilkayastha1490@gmail.com>
2022-02-28 14:08:31 +0545

    category completion

---
+++ db/migrate/20220228081954_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.decimal :cost_price
+      t.decimal :selling_price
+      t.integer :category_id
+      t.decimal :quantity
+      t.decimal :ideal_quantity
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
