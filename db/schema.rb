c09032c6c70aeb5c2f800692319471d3ce71451e
casper <salilkayastha1490@gmail.com>
2022-02-28 14:10:26 +0545

    model and controller of items

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_28_044420) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_28_081954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.decimal "cost_price"
+    t.decimal "selling_price"
+    t.integer "category_id"
+    t.decimal "quantity"
+    t.decimal "ideal_quantity"
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
