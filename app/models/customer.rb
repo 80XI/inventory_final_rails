9f6df6f49654227c2ffe7514cd1a553220d893cc
casper <salilkayastha1490@gmail.com>
2022-03-11 00:59:04 +0545

    Validation of customer and item

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ApplicationRecord
   has_many :sales
   has_many :items, through: :sales
 
+  validates :name, presence: true,
+  validates :phone_number, presence: true, length: { is: 10 }, uniqueness: true
 end
